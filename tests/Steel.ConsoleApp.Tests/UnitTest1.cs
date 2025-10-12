5edd5387170469b300f71df1bb17466224a99882
Steven Liekens <steven.liekens@gmail.com>
2025-10-12 13:08:53 +0000

    Add codestyle analyzer config

--- tests/Steel.ConsoleApp.Tests/UnitTest1.cs
+++ tests/Steel.ConsoleApp.Tests/UnitTest1.cs
@@ -1,7 +1,13 @@
 ﻿namespace Steel.ConsoleApp.Tests;
 
+/// <summary>
+/// Contains unit tests for the application.
+/// </summary>
 public class UnitTest1
 {
+    /// <summary>
+    /// Unit test for verifying a specific functionality.
+    /// </summary>
     [Fact]
     public void Test1()
     {
